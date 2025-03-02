1bf6e9baeec7b19273e9705abc4a679bd31bf672
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-01 21:33:39 -0800

    Fixing docker compose

--- docker_setup.sh
+++ docker_setup.sh
@@ -5,10 +5,9 @@ sudo yum install -y docker
 sudo service docker start
 sudo usermod -aG docker ec2-user
 
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
+mkdir -p "$DOCKER_CONFIG"/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.33.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x "$DOCKER_CONFIG"/cli-plugins/docker-compose
 
 docker compose version
 
--- ecr_steps.sh
+++ ecr_steps.sh
@@ -6,13 +6,14 @@ export LATEST_BACKEND_TAG=$(aws ecr describe-images --repository-name midterm/ec
     --query "reverse(sort_by(imageDetails[?starts_with(imageTags[0], 'backend_')], &imagePushedAt))[0].imageTags[0]" \
     --output text)
 
-echo "LATEST_BACKEND_TAG=$LATEST_BACKEND_TAG" >> $GITHUB_ENV
+echo "LATEST_BACKEND_TAG=$LATEST_BACKEND_TAG"
 
+# shellcheck disable=SC2155
 export LATEST_FRONTEND_TAG=$(aws ecr describe-images --repository-name midterm/ecr_1 \
     --query "reverse(sort_by(imageDetails[?starts_with(imageTags[0], 'frontend_')], &imagePushedAt))[0].imageTags[0]" \
     --output text)
 
-echo "LATEST_FRONTEND_TAG=$LATEST_FRONTEND_TAG" >> $GITHUB_ENV
+echo "LATEST_FRONTEND_TAG=$LATEST_FRONTEND_TAG"
 
 sudo docker pull 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_1:$LATEST_BACKEND_TAG
 
@@ -25,5 +26,7 @@ sudo docker tag 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_1:$LATE
   343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_1:frontend_latest
 
 mv ./docker-compose.ec2.yml ./docker-compose.yml
-export DOCKER_CONFIG=/home/ec2-user/.docker
+
+
+echo $(which docker-compose)
 sudo docker compose up -d
