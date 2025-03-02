79c8eae4083b3ef652669e7cf372166b981c1326
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-01 21:14:12 -0800

    fixing docker compose command

--- docker_setup.sh
+++ docker_setup.sh
@@ -10,5 +10,5 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.33.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
-docker compose --version
+docker compose version
 
