12f1c9cb9dd6298791b33174632c5cddfff3a4b1
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-02 14:25:41 -0800

    workflow_3 updated for image tagging

--- qa/pull_images.sh
+++ qa/pull_images.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 343830488876.dkr.ecr.us-east-1.amazonaws.com
 
+#fetch latest images tags available locally
 export FRONTEND_LATEST_LOCAL=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -E '^.*:frontend_' | awk -F: '{print $2}' | sort -V | head -n 1)
 
 echo "FRONTEND_LATEST_LOCAL=$FRONTEND_LATEST_LOCAL"
@@ -10,6 +12,7 @@ export BACKEND_LATEST_LOCAL=$(docker images --format "{{.Repository}}:{{.Tag}}" 
 
 echo "BACKEND_LATEST_LOCAL=$BACKEND_LATEST_LOCAL"
 
+#Fetchs latest image tags in ECR
 export LATEST_BACKEND_TAG=$(aws ecr describe-images --repository-name midterm/ecr_2 \
     --query "reverse(sort_by(imageDetails[?starts_with(imageTags[0], 'backend_')], &imagePushedAt))[0].imageTags[0]" \
     --output text)
@@ -25,6 +28,7 @@ echo "LATEST_FRONTEND_TAG=$LATEST_FRONTEND_TAG"
 
 if [[ "$LATEST_FRONTEND_TAG" != "$FRONTEND_LATEST_LOCAL" ]]; then
     docker pull 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:$LATEST_FRONTEND_TAG
+    docker tag 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:frontend_latest 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:$FRONTEND_LATEST_LOCAL
     docker tag 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:$LATEST_FRONTEND_TAG 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:frontend_latest
     # Command A
 else
@@ -32,6 +36,7 @@ else
 fi
 if [[ "$LATEST_BACKEND_TAG" != "$BACKEND_LATEST_LOCAL" ]]; then
     docker pull 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:$LATEST_BACKEND_TAG
+    docker tag 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:backend_latest 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:$BACKEND_LATEST_LOCAL
     docker tag 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:$LATEST_BACKEND_TAG 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:backend_latest
     # Command A
 else
