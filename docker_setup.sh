372afcbe9f8a1a5e5c2954506a29b3081153cb9d
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-01 19:55:51 -0800

    Changed docker setup instructions and updated image tagging in workflow_2.yml

--- docker_setup.sh
+++ docker_setup.sh
@@ -8,6 +8,7 @@ sudo usermod -aG docker ec2-user
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.33.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 docker compose --version
 
