0336721e472ccd3e7561034faf7b623b4d5e3262
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-01 22:18:20 -0800

    Fixing docker compose

--- ecr_steps.sh
+++ ecr_steps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 343830488876.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 343830488876.dkr.ecr.us-east-1.amazonaws.com
 
 export LATEST_BACKEND_TAG=$(aws ecr describe-images --repository-name midterm/ecr_1 \
     --query "reverse(sort_by(imageDetails[?starts_with(imageTags[0], 'backend_')], &imagePushedAt))[0].imageTags[0]" \
