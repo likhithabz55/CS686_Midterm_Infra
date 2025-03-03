41b444bb2ab865a4040e6fc3d0049cea4466d0d4
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-03 05:14:03 -0800

    fixing bugs in integration steps test

--- dev/image_push.sh
+++ dev/image_push.sh
@@ -1,6 +1,4 @@
-export FRONTEND_TIMESTAMP=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -E '^.*:frontend_' | awk -F: '{print $2}' | sort -V | head -n 1)
 
-export BACKEND_TIMESTAMP=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -E '^.*:backend_' | awk -F: '{print $2}' | sort -V | head -n 1)
 
 docker tag 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_1:$BACKEND_TIMESTAMP 343830488876.dkr.ecr.us-east-1.amazonaws.com/midterm/ecr_2:$BACKEND_TIMESTAMP
 
